1be404e7057262c8efd30b5a32bf016a7a01d95d
Oscar Rosello <rosello@Oscars-MacBook-Pro-2.local>
2019-08-29 21:19:52 -0400

    the bot makes some magic in the hrv and the bpms

--- wired vibration/HeartBit_Wired_Vibration_Embedded_1/HeartBit_Wired_Vibration_Embedded_1.ino
+++ wired vibration/HeartBit_Wired_Vibration_Embedded_1/HeartBit_Wired_Vibration_Embedded_1.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <SD.h>
+#include <cmath>
 
 //Definitions
 enum {
@@ -10,7 +11,8 @@ enum {
   
   SECOND_MS = 1000,
   MINUTE_MS = 60 * SECOND_MS,
-  BPM_PERIOD_MS = 5 * SECOND_MS, // Window size for calculating BPM over
+  BPM_LONG_PERIOD_MS = 15 * SECOND_MS, // Bigger window size for calculating BPM over
+  BPM_SHORT_PERIOD_MS = 5 * SECOND_MS, // Smaller window size for calculating BPM over
   BPM_REFRESH_PERIOD = 1 * SECOND_MS, // How frequently BPM is re-calculated
   MIN_TIME_BETWEEN_BEATS_MS = 100, // How long to wait between valid beat timestamps
   
@@ -26,46 +28,77 @@ typedef enum {
   BUZZ_OFF
 } buzz_state_t;
 
+long time_offset=0;
 byte last_sample=0;
-unsigned long last_bpm_time=0;
-unsigned long beatBuffer[BUFFER_SIZE];
-int beatIndex = BUFFER_SIZE - 1;
-bool initialized = false;
-unsigned long last_beat_time=0;
-unsigned long time_offset=0;
-buzz_state_t buzz_state = BUZZ_OFF;
-unsigned long buzz_last_updated = 0;
 
-int calculate_bpm(const unsigned long* buffer, const unsigned last_beat_index, const unsigned now) {  
-  int beats_in_last_period = 1;
-  int i;
-  // Start from last valid beat and search backwards for more
-  for (i = last_beat_index - 1; i >= 0; i--) {
-    const long diff = now - buffer[i];
-    if (diff > BPM_PERIOD_MS) {
-      // We're at beat timestamps that are too old, quit counting
-      break;
+long last_bpm_time=0;
+unsigned int current_bpm_long = 0;
+unsigned int current_bpm_short = 0;
+float current_hrv = 0.0f;
+long beatBuffer[BUFFER_SIZE];
+int beatIndex = BUFFER_SIZE - 1;
+long last_beat_time=0;
+
+buzz_state_t buzz_state = BUZZ_OFF;
+long buzz_last_updated = 0;
+
+int index_of_first_beat_in_window(const long buffer[BUFFER_SIZE], 
+                                  const unsigned last_beat_index,
+                                  const unsigned now,
+                                  const unsigned period_ms) {
+  // Start from previous beat and search backwards for more
+  for (int i = last_beat_index - 1; i >= 0; i--) {
+    const long unsigned diff = now - buffer[i];
+    if (diff > period_ms || buffer[i] == 0) {
+      // We're at beat timestamps that are too old, return last valid index
+      return i + 1;
     }
-    beats_in_last_period++;
   }
   // Start from end of buffer and keep searching backwards
-  for (i = BUFFER_SIZE - 1; i > last_beat_index; i--) {
-    const long diff =  now - buffer[i];
-    if (diff > BPM_PERIOD_MS) {
-      // Bail on encountering too-old timestamps
-      break;
+  for (int i = BUFFER_SIZE - 1; i > last_beat_index; i--) {
+    const long unsigned diff =  now - buffer[i];
+    if (diff > period_ms || buffer[i] == 0) {
+      // Bail on encountering too-old, or unfilled timestamps
+      return (i + 1) % BUFFER_SIZE;
     }
-    beats_in_last_period++;
   }
-  const int bpm = beats_in_last_period * MINUTE_MS / BPM_PERIOD_MS;
+  // We made it all the way around and all values were within window
+  return last_beat_index + 1;
+}
+
+int calculate_bpm(const long buffer[BUFFER_SIZE], const unsigned last_beat_index,
+                  const unsigned now, const unsigned period_ms) {
+  const int first_in_window = index_of_first_beat_in_window(buffer, last_beat_index,
+                                                            now, period_ms);
+  const int beats_in_last_period = (last_beat_index + BUFFER_SIZE - first_in_window + 1) % BUFFER_SIZE;
+  const int bpm = beats_in_last_period * MINUTE_MS / period_ms;
   return bpm;
 }
 
-unsigned long time_since_start(void){
-  const unsigned long now = millis();
-  if (time_offset == 0) { // should only set this value the first time
-    time_offset = now;
+float calculate_hrv_time(const long buffer[BUFFER_SIZE], const unsigned last_beat_index,
+                       const unsigned now, const unsigned period_ms) {
+  const int first_in_window = index_of_first_beat_in_window(buffer, last_beat_index,
+                                                            now, period_ms);
+  if ((first_in_window +1) % BUFFER_SIZE == last_beat_index) {
+    // Only one interval present, leave
+    return 0.0f;
   }
+  float current_average = 0.0f;
+  unsigned n = 0;
+  for (int i=first_in_window; (i % BUFFER_SIZE) != ((last_beat_index - 1) % BUFFER_SIZE); i++) {
+    const int j = i % BUFFER_SIZE;
+    const long interval_1 = buffer[j+1] - buffer[j];
+    const long interval_2 = buffer[j+2] - buffer[j+1];
+    const long interval_diff = interval_2 - interval_1;
+    const long interval_diff_squared = interval_diff * interval_diff;
+    n++;
+    current_average += (interval_diff_squared - current_average) / (float) n;
+  }
+  return sqrt(current_average);
+}
+
+long time_since_start(void){
+  const long now = millis();
   return now - time_offset;
 }
 
@@ -75,15 +108,18 @@ void setup() {
   pinMode (HR_RX, INPUT);  //Signal pin to input
   pinMode(MOTOR_PIN, OUTPUT);
 
-  // Wait until a heart beat is detected
-  while (!digitalRead(HR_RX));
   for(int i = 0; i < BUFFER_SIZE; i++) {
     beatBuffer[i] = 0;
     beatBuffer[i] = 0;
   }
+  // Wait until a heart beat is detected
+  while (!digitalRead(HR_RX));
+  
+  const long now = millis();
+  time_offset = now;
 }
 
-void execute_buzz(const unsigned long time){
+void execute_buzz(const long time){
   switch(buzz_state) {
     case BUZZ_STAGE_FIRST_BUZZ:
       if (time - buzz_last_updated > FIRST_BUZZ_MS) {
@@ -115,11 +151,11 @@ void execute_buzz(const unsigned long time){
 
 void loop() {
   // detection code
-  const unsigned long time = time_since_start();
+  const long time = time_since_start();
   const byte sample = digitalRead(HR_RX); // Store signal input
   if (sample && last_sample != sample && time - last_beat_time > MIN_TIME_BETWEEN_BEATS_MS) {
     last_beat_time = time;
-    Serial.println("Beat at " + String(time));
+    Serial.println(String(time) + "," + String(current_bpm_short) + "," + String(current_bpm_long) + "," + String(current_hrv));
     beatIndex = (beatIndex + 1) % BUFFER_SIZE;
     beatBuffer[beatIndex] = time;
 
@@ -128,9 +164,10 @@ void loop() {
   }
   last_sample = sample; //Store last signal received
 
-  if (time > BPM_PERIOD_MS && time - last_bpm_time >= BPM_REFRESH_PERIOD) {
-    const int bpm = calculate_bpm(beatBuffer, beatIndex, time);
-    Serial.println("Bpm: " + String(bpm));
+  if (time - last_bpm_time >= BPM_REFRESH_PERIOD) {
+    current_bpm_long = calculate_bpm(beatBuffer, beatIndex, time, BPM_LONG_PERIOD_MS);
+    current_bpm_short = calculate_bpm(beatBuffer, beatIndex, time, BPM_SHORT_PERIOD_MS);
+    current_hrv = calculate_hrv_time(beatBuffer, beatIndex, time, BPM_LONG_PERIOD_MS);
     last_bpm_time = time;
   }
 
